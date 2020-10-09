e2f2402df3a74b3e5de0c068abd69612d0f00bc1
“Author <Author>
2020-10-09 15:23:11 +0800

    page的运用

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,10 +1,54 @@
-const app=getApp()
-const name=app.globalData.name;
-const age=app.globalData.age;
+// const app=getApp()
+// const name=app.globalData.name;
+// const age=app.globalData.age;
 
 Page({
-  //获取用户的信息
+  //1获取用户的信息
   handleGetUserInfo(event){
-    console.log(event.detail);
+    console.log(event);
+  },
+  //2
+  data:{
+    list:[]
+  }, 
+  //3监听页面的生命周期函数
+  onLoad(){
+    console.log('onLoad')
+    wx.request({
+      url: 'url',
+      success:(res)=>{
+        console.log(res)
+        const data=res.rawData
+        this.setData({
+          list:data
+        })
+      }
+    })
+  },
+  //页面初次渲染完成的时候
+  onReady(){
+    console.log('onReady')
+  },
+  //页面显示出来
+  onShow(){
+    console.log('onShow')
+  },
+  onHide(){
+    console.log('onHide')
+  },
+  onUnload(){
+    console.log('onUnload')
+  },
+  //4监听其他事件
+  //页面的滚动
+  onPageScroll(obj){
+    console.log(obj)
+  },
+  //监听页面的底部
+  onReachBottom(){
+    console.log("到底部了")
+  },
+  onPullDownRefresh(){
+    console.log("下拉刷新的事件")
   }
 })
